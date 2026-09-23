1df3bd440465619939d8d0c42d7bb69e92276c3a
Fedor <fedor@MacBook-Pro-Fedor.local>
2026-09-23 21:04:42 +0300

    Задание 5

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from utils.statistics import *
 from utils.reader import *
 from pathlib import Path
 
+file_path = Path("data") / "measurements.txt"
+
+print(Path.cwd())
 
 def main():
-    lst = read_numbers('data/measurements.txt')
+    if file_path.exists():
+        lst = read_numbers(file_path)
 
-    print(calculate_average(lst))
-    print(find_max(lst))
-    print(find_min(lst))
+        print(calculate_average(lst))
+        print(find_max(lst))
+        print(find_min(lst))
+    else:
+        print('Файла не существует')
 
 if __name__ == "__main__": 
     main()
+
+Path("logs").mkdir(exist_ok=True)
