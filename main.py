438c4d7efb6e4b09da52ce8ccd6b859fb6564c28
Fedor <fedor@MacBook-Pro-Fedor.local>
2026-09-23 20:13:43 +0300

    Задание 3

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from utils.statistics import *
 
 
-lst = [1, 2, 4, 5, 88, 43, 2, 457, 77654, -6]
+def main():
+    lst = [1, 2, 4, 5, 88, 43, 2, 457, 77654, -6]
 
-print(calculate_average(lst))
-print(find_max(lst))
-print(find_min(lst))
+    print(calculate_average(lst))
+    print(find_max(lst))
+    print(find_min(lst))
+
+if __name__ == "__main__": 
+    main()
