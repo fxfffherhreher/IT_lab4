8eaf73170d7a716182a41cab1b55f2bae44cf9dc
Fedor <fedor@MacBook-Pro-Fedor.local>
2026-09-23 21:34:27 +0300

    Задание 6

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ from pathlib import Path
 
 
 def main():
-    file_path = Path("data") / "measurements.txt"
-    if file_path.exists():
-        lst = read_numbers(file_path)
+    try:
+        file_path = Path("data") / "measurements.txt"
+    except FileNotFoundError:
+        print('Файла не существует')
 
-        print(calculate_average(lst))
-        print(find_max(lst))
-        print(find_min(lst))
+    if file_path.exists():
+        try:
+            lst = read_numbers(file_path)
+
+            print(calculate_average(lst))
+            print(find_max(lst))
+            print(find_min(lst))
+        except ValueError:
+            print('В файле не только числа')
+    
     else:
         print('Файла не существует')
 
