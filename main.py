cece494d64d53450b39b96eb3645c73f7e90a835
Fedor <fedor@MacBook-Pro-Fedor.local>
2026-09-24 13:15:19 +0300

    Задание 8

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 from utils.statistics import *
 from utils.reader import *
 from pathlib import Path
+import logging
 
 
 def main():
+    Path("logs").mkdir(exist_ok=True)
+    
+    logging.basicConfig(
+        filename="logs/app.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+
+    logging.info('Программа запущена')
+
     try:
         file_path = Path("data") / "measurements.txt"
     except FileNotFoundError:
+        logging.ERROR('Файла не существует')
         print('Файла не существует')
 
     if file_path.exists():
         try:
             lst = read_numbers(file_path)
+            logging.info('Файл успешно прочтен')
+            logging.info('Значений прочтено: %s', len(lst))
         except ValueError:
             print('В файле не только числа')
+            logging.ERROR('В файле не только числа')
         try:
             validate_numbers(lst)
             print(calculate_average(lst))
@@ -21,6 +36,7 @@ def main():
             print(find_min(lst))
         except ValueError as e:
             print(e)
+            logging.error('Список измерений пуст')
 
         
     else:
@@ -28,8 +44,7 @@ def main():
 
     print(Path.cwd())
 
-    Path("logs").mkdir(exist_ok=True)
-
+    logging.info('Конец программы')
 if __name__ == "__main__": 
     main()
 
